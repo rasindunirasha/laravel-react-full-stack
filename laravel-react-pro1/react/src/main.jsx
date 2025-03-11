f35debe6be98df4e9814b42fadd8e73d8e70baba
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-11 14:38:37 +0530

    import index.css file

--- laravel-react-pro1/react/src/main.jsx
+++ laravel-react-pro1/react/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router";  // ✅ Import Router
 import { ContextProvider } from "./contexts/ContextProvider";
+import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
