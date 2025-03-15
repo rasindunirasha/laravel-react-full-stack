862d485e12ba65d014e4aa40a4774389adfbba52
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-15 12:06:30 +0530

    create signup.jsx

--- laravel-react-pro1/react/src/views/Signup.jsx
+++ laravel-react-pro1/react/src/views/Signup.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom"; 
 
 const Signup = () => {
+
+
+    const onSubmit = (ev) => {
+      ev.preventDefault();
+    };
+
+
   return (
-    <div>
-      <h1>Signup Page</h1>
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}> 
+          <h1 className="title">Signup for free</h1>
+          <input type="email" placeholder="Full Name" required />
+          <input type="email" placeholder="Email Address" required />
+          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Password Confirmation" required />
+          <button className="btn btn-block">Signup</button> 
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link> 
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
